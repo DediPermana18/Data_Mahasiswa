62974b3a36e5001ecbef9940f1f52bc7516530c1
Dedi7011ZX <dedipermana7011@gmail.com>
2025-10-18 17:32:58 +0700

    Refactor functions and improve formatting

--- DataMahasiswaVoid.cpp
+++ DataMahasiswaVoid.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
 using namespace std;
 
-double nilairata(int uts, int uas, int tugas) {
-    return (tugas * 0.25) + (uts * 0.35) + (uas * 0.4);
-}
-double nilaibobotakhirUTS(int uts) {
-    return uts * 0.35;
-}
-double nilaibobotakhirUAS(int uas) {
-    return uas * 0.4;
-}
-double nilaibobotakhirTugas(int tugas) {
-    return tugas * 0.25;
-}
+double nilairata(int uts, int uas, int tugas) {return (tugas * 0.25) + (uts * 0.35) + (uas * 0.4);}
+double nilaibobotakhirUTS(int uts) {return uts * 0.35;}
+double nilaibobotakhirUAS(int uas) {return uas * 0.4;}
+double nilaibobotakhirTugas(int tugas) {return tugas * 0.25;}
 
-struct tanggal {
-    int hari;
-    int bulan;
-    int tahun;
-};
-struct nilai {
-    int uts;
-    int uas;
-    int tugas;
-};
+struct tanggal { int hari; int bulan; int tahun; };
+struct nilai { int uts; int uas; int tugas; };
 struct Mahasiswa {
     string nama;
     int umur;
@@ -31,7 +15,6 @@ struct Mahasiswa {
     tanggal lahir;
     nilai nilaiMhs;
 };
-
 int jumlahMahasiswa() {
     int jumlahMhs;
     cout << "Masukkan jumlah mahasiswa: ";
@@ -50,7 +33,6 @@ void mahasiswaInfo(Mahasiswa &mhs) {
     getline(cin, mhs.jurusan);
     cout << endl;
 }
-
 void inputTanggal(tanggal &tgl) {
     cout << "Masukkan tanggal lahir: " << endl;  
     cout << "Tanggal: ";
@@ -61,7 +43,6 @@ void inputTanggal(tanggal &tgl) {
     cin >> tgl.tahun;
     cout << endl;
 }
-
 void inputNilai(nilai &nilaiMhs) {
     cout << "Masukkan nilai UTS: ";
     cin >> nilaiMhs.uts;
@@ -71,7 +52,6 @@ void inputNilai(nilai &nilaiMhs) {
     cin >> nilaiMhs.tugas;
     cout << endl;
 }
-
 void bobotNilai(const nilai &nilaiMhs) {
     cout << "Bobot Nilai:" << endl;
     cout << "UTS   : " << nilaibobotakhirUTS(nilaiMhs.uts) << endl;
@@ -79,43 +59,35 @@ void bobotNilai(const nilai &nilaiMhs) {
     cout << "Tugas : " << nilaibobotakhirTugas(nilaiMhs.tugas) << endl;
     cout << endl;
 }
-
 void printMahasiswaData(const Mahasiswa &mhs) {
-    cout << "Nama: " << mhs.nama << endl;
-    cout << "Umur: " << mhs.umur << endl;
+    cout << "Nama   : " << mhs.nama << endl;
+    cout << "Umur   : " << mhs.umur << endl;
     cout << "Jurusan: " << mhs.jurusan << endl << endl;
-
     cout << "      ** Tanggal Lahir **" << endl;
     cout << "   tanggal " << mhs.lahir.hari << " /bulan " << mhs.lahir.bulan << " /tahun " << mhs.lahir.tahun << endl << endl;
-
     cout << "          ** Nilai **" << endl;
-    cout << "UTS: " << mhs.nilaiMhs.uts << endl;
-    cout << "UAS: " << mhs.nilaiMhs.uas << endl;
-    cout << "Tugas: " << mhs.nilaiMhs.tugas << endl << endl;
-    cout << "          ** Bobot **" << endl;
+    cout << "UTS   : " << mhs.nilaiMhs.uts << endl;
+    cout << "UAS   : " << mhs.nilaiMhs.uas << endl;
+    cout << "Tugas : " << mhs.nilaiMhs.tugas << endl;
     bobotNilai(mhs.nilaiMhs);
     cout << "Nilai Rata-rata: " 
          << nilairata(mhs.nilaiMhs.uts, mhs.nilaiMhs.uas, mhs.nilaiMhs.tugas) << endl;
     cout << "===============================" << endl;
 }
-
 int main() {
     int jumlahMhs = jumlahMahasiswa();
     Mahasiswa mhs[jumlahMhs];
 
     for (int i = 0; i < jumlahMhs; i++) {
-        cout << "     **input Mahasiswa ke-" << i + 1 << "**" << endl;
-        
+        cout << "     **input Mahasiswa ke-" << i + 1 << "**" << endl; 
         mahasiswaInfo(mhs[i]);
         inputTanggal(mhs[i].lahir);
         inputNilai(mhs[i].nilaiMhs);
     }
-
     for (int i = 0; i < jumlahMhs; i++) {
         cout << "===============================" << endl;
         cout << "    **Data Mahasiswa ke-" << i + 1 << "**" << endl;
         printMahasiswaData(mhs[i]);
     }
-
     return 0;
 }
